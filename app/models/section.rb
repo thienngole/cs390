84227a27ea9dfc00e99bafae1c26922d45481321
ThienNgo Le <tle61@msudenver.edu>
2018-10-08 17:39:27 -0600

    fix validations and update css for nav-bar

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,5 +2,7 @@ class Section < ApplicationRecord
   belongs_to :course
   has_and_belongs_to_many :students
 
+  validates :number, presence: true
+  validates :semester, presence: true
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,8 @@
 class Student < ApplicationRecord
 	has_and_belongs_to_many :sections
+
+	validates :name, presence: true
+	validates :name, uniqueness: true
+	
+
 end
